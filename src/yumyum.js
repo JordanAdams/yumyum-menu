2f97cf3aa98c3b4149dcebff7fe448d92bdbbd5a
Jordan Adams <hi@jordanadams.co.uk>
2019-10-27 19:54:47 +0000

    adds feature flag for date filter

--- src/yumyum.js
+++ src/yumyum.js
@@ -1,15 +1,19 @@
 const { DateTime } = require("luxon");
 const { findEmail } = require("./gmail");
 const cheerio = require("cheerio");
+const config = require("./config");
 
 const LOGO_URL =
   "https://gallery.mailchimp.com/960535dd9067bb938d37f961b/images/36ca8e6b-70cc-4ab5-82ca-952c29e8b00c.png";
 
 const fetchLatestMenuEmail = () => {
   const today = DateTime.local().toFormat("yyyy/MM/dd");
-  const q = `from:cafe.yumyum@outlook.com after:${today}`;
+  const filters = [
+    "from:cafe.yumyum@outlook.com",
+    config.features.dateFilter ? `after:${today}` : ""
+  ];
 
-  return findEmail(q);
+  return findEmail(filters.join(""));
 };
 
 const extractMenuImageUrl = html => {
